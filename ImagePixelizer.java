f9da154111fc2028dbec97090cceea2c7cd97b31
Dursun <dursun.dashdemirov.std@bhos.edu.az>
2023-11-12 00:09:26 +0400

    Seperating the Main class

--- ImagePixelizer.java
+++ ImagePixelizer.java
@@ -164,22 +164,4 @@ public class ImagePixelizer {
         g2d.drawImage(originalImage, 0, 0, scaledImage.getWidth(), scaledImage.getHeight(), null);
         g2d.dispose();
     }
-
-    public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out
-                    .println("Input format should be: java ImagePixelizer <filename> <square size> <processing mode>");
-            System.exit(1);
-        }
-
-        try {
-            String fileName = args[0];
-            int squareSize = Integer.parseInt(args[1]);
-            char processingMode = args[2].charAt(0);
-            ImagePixelizer pixelizer = new ImagePixelizer(fileName, squareSize, processingMode);
-            pixelizer.processImage();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out
+                    .println("Input format should be: java ImagePixelizer <filename> <square size> <processing mode>");
+            System.exit(1);
+        }
+
+        try {
+            String fileName = args[0];
+            int squareSize = Integer.parseInt(args[1]);
+            char processingMode = args[2].charAt(0);
+            ImagePixelizer pixelizer = new ImagePixelizer(fileName, squareSize, processingMode);
+            pixelizer.processImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
